d5b71d7e3c0a124fe3f64cbf97a77d915dbd6890
rikzabk <rikzabk@gmail.com>
2021-10-20 18:45:33 +0700

    coab buat toolbar custom

---
+++ app/src/main/java/com/example/worqd/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.worqd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toolbar
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/example/worqd/LoginActivity.kt
+++ app/src/main/java/com/example/worqd/LoginActivity.kt
@@ -1,11 +1,20 @@
 package com.example.worqd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btn: Button = findViewById(R.id.button)
+
+        btn.setOnClickListener {
+            val intent= Intent(this@LoginActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
