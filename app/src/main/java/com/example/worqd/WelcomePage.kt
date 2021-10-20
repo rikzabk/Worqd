01b78012c586f425bb8b109905fda60c89bc33bf
rikzabk <rikzabk@gmail.com>
2021-10-20 17:01:28 +0700

    Initial commit incl splash screen and welcome page

---
+++ app/src/main/java/com/example/worqd/WelcomePage.kt
@@ -0,0 +1,23 @@
+package com.example.worqd
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+
+class WelcomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_page)
+
+        supportActionBar?.hide()
+
+        val wp_btn: ImageButton = findViewById(R.id.welcome_page_button)
+
+        wp_btn.setOnClickListener {
+            val intent2 = Intent(this@WelcomePage, LoginActivity::class.java)
+            startActivity(intent2)
+            finish()
+        }
+    }
+}
