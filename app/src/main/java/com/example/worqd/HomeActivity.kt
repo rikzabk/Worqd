367501da1767a163b89a99cfeced9929e98cdeff
rikzabk <rikzabk@gmail.com>
2021-10-24 01:06:55 +0700

    recycler card

--- app/src/main/java/com/example/worqd/HomeActivity.kt
+++ app/src/main/java/com/example/worqd/HomeActivity.kt
@@ -2,11 +2,16 @@ package com.example.worqd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toolbar
+import androidx.appcompat.widget.Toolbar
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val hometoolbar: Toolbar = findViewById(R.id.homeToolbar)
+        setSupportActionBar(hometoolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
     }
 }
